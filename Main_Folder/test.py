1e0f69e93621f1e8431955cbf77b6e39dd2015de
adnanmumtaz <adnan.uiit@gmail.com>
2024-08-30 21:38:42 +0500

    Second Commit

--- Main_Folder/test.py
+++ Main_Folder/test.py
@@ -1,1 +1,25 @@
-print("Hello World")
+print("Welcome to Quiz App")
+score = 0
+answer1 = input("What does CPU stands for: ")
+if answer1 == "Central Processing Unit":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+answer2 = input("What does ALU stands for: ")
+if answer2 == "Arithematic and Logic Unit":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+answer3 = input("Who is PM of Pakistan: ")
+if answer3 == "Shahbaz Sharif":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+print("Your score is:", score)
+print("You got " + str(((score / 3) * 100)) + " percent")
